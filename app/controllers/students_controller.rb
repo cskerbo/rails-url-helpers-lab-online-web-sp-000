17f093f401eacaf8f289279f70a79044b07a5a03
Chris Kerbo <chris.kerbo@outlook.com>
2020-01-10 18:59:06 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,15 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
+  after_initialize :init
 
   def index
     @students = Student.all
   end
 
+  def init
+    self.active = false if self.active.nil?
+  end
+  
   def show
     @student = Student.find(params[:id])
   end
