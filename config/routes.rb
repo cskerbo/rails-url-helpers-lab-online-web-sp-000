ebef6320c91a3268e6734913ee99c11ec4fcd603
Chris Kerbo <chris.kerbo@outlook.com>
2020-01-10 19:39:36 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/students', to: 'students#index'
   get '/students/:id', to: 'students#show', as: 'student'
+  get '/students/:id/activate', to: 'students#activate', as: 'activate_student'
 end
